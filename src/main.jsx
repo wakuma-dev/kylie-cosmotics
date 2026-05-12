3996ac7477fedfedb5002dd061b7a3b5847e2719
wakuma-dev <wakuma.hailu.dev@gmail.com>
2026-05-12 15:43:11 +0300

    fix github routing

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import AuthProvider from './context/AuthProvider.jsx'
 import SearchProvider from './context/SearchProvider.jsx'
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/kylie-cosmotics/">
       <AuthProvider>
         <RouteProvider>
           <CartProvider>
